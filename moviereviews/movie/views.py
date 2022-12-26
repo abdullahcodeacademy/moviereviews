ad3b55b587087b29e8d05c491a2507ba94551d5c
abdullahcodeacademy <abdullahcodeacademy@gmail.com>
2022-12-26 23:25:00 +0000

    add movie model and feature

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    context = {
+        'searchTerm': searchTerm,
+        'movies': movies
+    }
+    return render(request, 'home.html', context)
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
+
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'movie/signup.html', {'email':email})
