ec567f3b3605887ef8c18d5b1dd0093780b4b5d6
abdullahcodeacademy <abdullahcodeacademy@gmail.com>
2022-12-27 02:18:52 +0000

    add review functionality

--- moviereviews/accounts/views.py
+++ moviereviews/accounts/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
+from django.db import IntegrityError
+from .forms import UserCreateForm
 def signupaccount(request):
     if request.method == 'GET':
-        return render(request, 'signupaccount.html', {'form':UserCreationForm})
+        return render(request, 'accounts/signupaccount.html', {'form':UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], 
-              password= request.POST['password1'])               
-            user.save()
-            login(request, user)
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'accounts/signupaccount.html', {'form':UserCreateForm, 'error':'Username already taken. Choose new username.'})
+        else:
+            return render(request, 'accounts/signupaccount.html', 
+             {'form':UserCreateForm, 
+             'error':'Passwords do not match'})
+
+
+def loginaccount(request):    
+    if request.method == 'GET':
+        return render(request, 'accounts/loginaccount.html', {'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])  
+        if user is None:
+            return render(request,'accounts/loginaccount.html', {'form': AuthenticationForm(), 'error': 'username and password do not match'})
+        else: 
+            login(request,user)
             return redirect('home')
+
+
+def logoutaccount(request):        
+    logout(request)
+    return redirect('home')
--- moviereviews/movie/models.py
+++ moviereviews/movie/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Movie(models.Model):
@@ -7,3 +7,16 @@ class Movie(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
+
+class Review(models.Model):
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    watchAgain = models.BooleanField()
+
+    def __str__(self):
+        return self.text 
+    
+    class Meta:
+        ordering = ['-date']
